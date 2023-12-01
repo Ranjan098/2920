dc833f4525d77f8868518f48f1a0eccbbcd5f32b
Jaatulendi <150812541+Jaatulendi@users.noreply.github.com>
2023-12-01 20:55:02 +0530

    Update details.py

--- details.py
+++ details.py
@@ -2,7 +2,7 @@ import os
 
 api_id = 20088962
 api_hash = "257f47d347157555890a64b12bc0134f"
-bot_token = "6778587387:AAFt5GDWxQjXXM6JX6MB1TTNKYPDivrvK4k"
+bot_token = "6743557600:AAGdbZ-rgsgGBm85QXofKeUYBfk5fpRAv8I"
 auth_users = [6169016546]
 sudo_user = 6169016546
 log_channel = -1002059297194
