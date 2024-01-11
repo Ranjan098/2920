ddd4b68c45567c071e613d03d216c805b8c977ab
Jaatulendi <150812541+Jaatulendi@users.noreply.github.com>
2024-01-12 01:01:35 +0530

    Update details.py

--- details.py
+++ details.py
@@ -5,5 +5,5 @@ api_hash = "257f47d347157555890a64b12bc0134f"
 bot_token = "6389756670:AAHXFERgeoJ_VjtvKx38M1ivGnsvoRDsLuM"
 auth_users = [6169016546]
 sudo_user = 6169016546
-log_channel = -1002059297194
+log_channel = -1001784009370
 txt_channel = -1002082838226
