d93f9c4e3ca0ab77b543eedf9b2b4125b0fda46d
Jaatulendi <150812541+Jaatulendi@users.noreply.github.com>
2024-02-06 19:47:30 +0530

    Update details.py

--- details.py
+++ details.py
@@ -2,7 +2,7 @@ import os
 
 api_id = 20088962
 api_hash = "257f47d347157555890a64b12bc0134f"
-bot_token = "6389756670:AAHXFERgeoJ_VjtvKx38M1ivGnsvoRDsLuM"
+bot_token = "6134747255:AAFnOrKgIGlQ4lORCmonc6sE_aYm8ASmsJY"
 auth_users = [6169016546]
 sudo_user = 6169016546
 log_channel = -1001784009370
