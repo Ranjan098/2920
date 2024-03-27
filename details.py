c7b6d5df210b57f40e53af084c5623a33fec0511
Ranjan098 <144373098+Ranjan098@users.noreply.github.com>
2024-03-27 20:56:21 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,9 +1,9 @@
 import os
 
-bot_token= "6746876285:AAGnUDNwDvWu4RfbkSSiQe8gt-2lU-9p-Bk"
-api_id= 20088962
-api_hash= "257f47d347157555890a64b12bc0134f"
-auth_users = [6169016546]
-sudo_user = 6169016546
-log_channel = -1001784009370
-txt_channel = -1002082838226
+bot_token= "6611699449:AAEM9JWzvoKhcxBayiFjvynFfOO97VBwq5Q"
+api_id= 12606917
+api_hash= "f25113b8c17dca6fa7abda53a86bd4f7"
+auth_users = [5318243282]
+sudo_user = 5318243282
+log_channel = -1001699711548
+txt_channel = -1001699711548
